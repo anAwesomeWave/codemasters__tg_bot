6bc60aadb0bb433c5e73449527590b039934db77
Tim Zubov <timofey.zubov.05@gmail.com>
2023-09-14 18:46:14 +0300

    proper fallbacks handlings and updating the fields. also start implementing finding employee by id

--- core/bot.py
+++ core/bot.py
@@ -25,7 +25,7 @@ WORKER_DATA = range(1)
 ENTER_ID, UPDATE_FIELD, UPDATE_VAL = range(3)
 
 UPDATE_END_VAL = range(1)
-
+FIND_ID = range(1)
 NUMBER_OF_REQUIRED_FIELDS = 4
 
 default_markup = ReplyKeyboardMarkup([
@@ -34,7 +34,6 @@ default_markup = ReplyKeyboardMarkup([
 
 
 def worker_data(update, _):
-    print('МЫ ТУТ')
     empl_data = update.message.text.split()
     if len(empl_data) != NUMBER_OF_REQUIRED_FIELDS:
         update.message.reply_text(
@@ -65,7 +64,10 @@ def worker_data(update, _):
 def update_enter_id(update, context):
     update.message.reply_text(
         'Введите ID сотрудника, которого вы хотите изменить.')
-    id = int(update.message.text.strip())
+    try:
+        id = int(update.message.text.strip())
+    except ValueError:
+        update.message.reply_text('Введите число')
     found_employee = core.db.get_user_by_id(id)
     if found_employee is None:
         update.message.reply_text(
@@ -101,24 +103,15 @@ def update_text_field(update, context):
     query = update.callback_query
 
     query.answer()
-    # query.edit_message_text(text=f"Selected option: {query.data}")
     data = query.data.split()
-    context.bot.send_message(
+    send_message(
+        context.bot,
         update.effective_chat.id,
         f'Вы хотите поменять поле {data[1]} у работника с ID {data[0]}\n'
         f'Введите новое значение или команду /cancel для отмены'
     )
     context.user_data['id'] = data[0]
     context.user_data['field_to_update'] = data[1]
-    # FIELD_TO_UPDATE = update.message.text.split()[-1]
-    # if FIELD_TO_UPDATE.lower() not in [x.lower() for x in ALL_DB_COLUMNS if x not in ('ID', 'avatar')]:
-    #     update.message.reply_text('Поле указано неверно',
-    #                               reply_markup=default_markup)
-    #     return ConversationHandler.END
-    # update.message.reply_text(f'Вы будете менять поле {FIELD_TO_UPDATE}\n'
-    #                           f'Введите новое значение этого поля',
-    #                           reply_markup=default_markup)
-    #
     return UPDATE_END_VAL
 
 
@@ -128,13 +121,12 @@ def update_avatar(update, context):
 
 
 def update_text_field_val(update, context):
-    update.message.reply_text(f"{context.user_data['id']} {context.user_data['field_to_update']}")
-    # new_val = update.message.text
-    # status = core.db.update_field(ID, {FIELD_TO_UPDATE: new_val})
-    # if status == 0:
-    #     update.message.reply_text('Вы изменили поле!')
-    # else:
-    #     update.message.reply_text('Что-то не так...')
+    new_val = update.message.text
+    status = core.db.update_field(context.user_data['id'], {context.user_data['field_to_update']: new_val})
+    if status == 0:
+        update.message.reply_text('Вы изменили поле!')
+    else:
+        update.message.reply_text('Что-то не так...')
     return ConversationHandler.END
 
 
@@ -156,22 +148,21 @@ def prepare_bot():
     add_user_conv = ConversationHandler(
         entry_points=[CommandHandler('add_employee', add_employee)],
         states={
-            WORKER_DATA: [MessageHandler(Filters.text, worker_data)],
+            WORKER_DATA: [MessageHandler(Filters.text & (~ Filters.command), worker_data)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel_conv)]
+    )
+    find_by_id_conv = ConversationHandler(
+        entry_points=[CommandHandler('find_by_id', send_find_by_id)],
+        states={
+            FIND_ID: [MessageHandler(Filters.text & (~ Filters.command), send_find_by_id_end)],
         },
         fallbacks=[CommandHandler('cancel', cancel_conv)]
     )
     update_employee_field_start = ConversationHandler(
         entry_points=[CommandHandler('update_employee', update_employee)],
         states={
-            ENTER_ID: [MessageHandler(Filters.text, update_enter_id)],
-            # UPDATE_FIELD: [
-            #     CommandHandler('update', update_text_field),
-            #     CommandHandler('update_avatar', update_avatar),
-            # ],
-            # UPDATE_VAL: [
-            #     MessageHandler(Filters.text, update_text_field_val),
-            #     MessageHandler(Filters.text, update_avatar_val),
-            # ]
+            ENTER_ID: [MessageHandler(Filters.text & (~ Filters.command), update_enter_id)],
 
         },
         fallbacks=[CommandHandler('cancel', cancel_conv)]
@@ -180,17 +171,16 @@ def prepare_bot():
         entry_points=[CallbackQueryHandler(update_text_field)],
         states={
             UPDATE_END_VAL: [
-                MessageHandler(Filters.text, update_text_field_val),
-                # CommandHandler(Filters.photo, update_avatar),
+                MessageHandler(Filters.text & (~ Filters.command), update_text_field_val),
             ],
 
         },
         fallbacks=[CommandHandler('cancel', cancel_conv)]
     )
     updater.dispatcher.add_handler(add_user_conv)
-    # updater.dispatcher.add_handler(CallbackQueryHandler(update_text_field))
     updater.dispatcher.add_handler(update_employee_field_start)
     updater.dispatcher.add_handler(update_employee_field_end)
+    updater.dispatcher.add_handler(find_by_id_conv)
     return updater
 
 
@@ -209,15 +199,30 @@ def add_employee(update, context):
     update.message.reply_text(
         'Добавление работника.'
         'Введите данные сотрудника через пробел( его имя, фамилию, занимаеую им должность и проект, над которым он работает).'
-        'Остальные данные вы сможете ввести позже.'
+        'Остальные данные вы сможете ввести позже. Также вы моежете ввести /cancel для отмены'
     )
     return WORKER_DATA
 
+def send_find_by_id(update, context):
+    update.message.reply_text('Введите ID сотрудника для продеолжения или cancel для отмены')
+    return FIND_ID
 
+def send_find_by_id_end(update, context):
+    try:
+        id = int(update.message.text)
+        found_employee = core.db.get_user_by_id(id)
+        if found_employee is not None:
+            update.message.reply_text(found_employee)
+    except ValueError as e:
+        update.message.reply_text("Введите число")
+    except Exception as e:
+        logger.error(e)
+
+    return ConversationHandler.END
 def update_employee(update, context):
     update.message.reply_text(
         'Обновление данных работника.'
-        'Введите ID работника.'
+        'Введите ID работника. Или /cancel для отмены'
     )
     return ENTER_ID
 
